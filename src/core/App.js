5af4f8da9b8c7ed261e67cc11a5ac0990d0e6240
kjh9589 <jeonghwan9589@gmail.com>
2023-04-17 01:09:59 +0900

    feat/React만들기

---
+++ src/core/App.js
@@ -0,0 +1,43 @@
+import { render, createElement } from "./React.js";
+
+// const virtualDom = {
+//   tag: "p",
+//   props: {},
+//   children: [
+//     {
+//       tag: "h1",
+//       props: {
+//         style: "color: red"
+//       },
+//       children: ["React 만들기"],
+//     },
+//   ],
+// };
+
+const test = () => {
+  return createElement("h2", {}, "야호~!!");
+};
+
+const testImg =
+  "https://cdn.pixabay.com/photo/2023/04/03/11/45/water-7896610_640.jpg";
+
+const virtualDom = createElement(
+  "p",
+  {},
+  createElement("h1", {}, "React 만들기"),
+  createElement(
+    "ul",
+    {},
+    createElement(
+      "li",
+      { id: "test", onClick: `console.log("???")` },
+      "첫번째 아이템"
+    ),
+    createElement("li", { style: "color: green" }, "두번째 아이템"),
+    createElement("li", { style: "color: blue" }, "세번째 아이템")
+  ),
+  test(),
+  createElement("img", { src: `${testImg}` }, "")
+);
+
+render(virtualDom, document.querySelector("#root"));
---
+++ src/core/React.js
@@ -0,0 +1,28 @@
+const createDom = (node) => {
+  // 하위 요소가 객체가 아니고 문자열일 때
+  if (typeof node === "string") {
+    return document.createTextNode(node);
+  }
+  const element = document.createElement(node.tag);
+
+  // props 처리
+  Object.entries(node.props).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+
+  // 재귀로 구현 => depth 모름
+  // context 고정을 위해 bind
+  node.children.map(createDom).forEach(element.appendChild.bind(element));
+
+  return element;
+};
+
+const render = (virtualDom, container) => {
+  container.appendChild(createDom(virtualDom));
+};
+
+const createElement = (tag, props, ...children) => {
+  return { tag, props, children };
+};
+
+export { createDom, render, createElement };
